4e378be8a0a2a45aa22c838f992e0a84d517393d
LucasTCamp <lucastcampbell@outlook.com>
2025-01-14 15:57:52 -0800

    All the crap

---
+++ index.js
@@ -0,0 +1,66 @@
+console.log("Internal Js console message. Hello, Bro")
+console.log(document)
+// alert("Hello world")
+// window.onload = function(){
+//     alert("Hello, Class")
+// }
+
+
+// prompts //
+// const student = prompt("What is your name?");
+// console.log(student);
+
+
+
+
+// const student = prompt("What is your name?");
+// const messageElement = document.createElement("p");
+// messageElement.textContent = `Hello, ${student}!`;
+// document.body.appendChild(messageElement);
+// document.write(' <p>Hello, ${student}!'</p>);
+
+
+// document.getElementById("myH1").innerHTML = "I love JavaScript" ;
+// document.body.style.color = "green"
+// document.body.style.backgroundColor = "black"
+// document.body.style.fontSize = "2em"
+
+
+// EVENT LISTENERS //
+
+
+document.getElementById("myH1").addEventListener("click", function(){
+    // function details go here.
+    document.getElementById("myH1").style.color = "red"
+});
+
+
+document.getElementById("myH1").addEventListener("mouseover", function(){
+    // function details go here.
+    document.getElementById("myH1").style.color = "orange"
+});
+
+
+document.getElementById("myH1").addEventListener("mouseover", function(){
+    // function details go here.
+     document.getElementById("myH1").style.fontSize = "50px"
+});
+
+
+
+
+document.getElementById("myParagraph").addEventListener("mouseover", function(){
+    // function details go here.
+    document.getElementById("myParagraph").style.color = "green"
+});
+
+
+document.getElementById("myParagraph").addEventListener("mouseover", function(){
+    // function details go here.
+    document.getElementById("myParagraph").style.fontSize = "18px"
+});
+
+
+document.getElementById("myButton").addEventListener("click", function(){
+    document.body.style.backgroundColor = "Black"
+});
